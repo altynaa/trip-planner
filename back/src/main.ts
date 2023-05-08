0705fe41286f09cb4db418606dece1083cff01af
Altyn Almazbekova <almazbekova.altyn@gmail.com>
2023-05-08 15:26:53 +0600

    Backend: configured global pipes for validation of errors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        const validationErrors: ValidationErrors = {};
+
+        errors.forEach((error) => {
+          const constraints = error.constraints;
+
+          if (constraints) {
+            validationErrors[error.property] = Object.keys(constraints).map(
+              (key) => constraints[key],
+            );
+          }
+        });
+        return new BadRequestException(validationErrors);
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  await app.listen(8000);
 }
 bootstrap();
